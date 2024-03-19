f97240e3dddfeea2eb80df3d3b08c3a7527f11dd
Bar Kimchi <bar.a.kimchi@gmail.com>
2024-03-18 19:47:50 -0700

    add flask 01 files

--- flask-_01_02_Jinja_Template/flask-01-hello-world-app/hello-world-app.py
+++ flask-_01_02_Jinja_Template/flask-01-hello-world-app/hello-world-app.py
@@ -1,6 +1,22 @@
 from flask import Flask 
+
 app = Flask(__name__)
 
+@app.route('/')
+def hello():
+    return ("Hello World")
+
+@app.route('/second')
+def second():
+    return("This is the second page")
+
+@app.route('/third/subthird')
+def third():
+    return("This is the subpage of third page")
+
+@app.route('/forth/<string:id>')
+def forth(id):
+    return f"The page id is {id}"
 
 
 
@@ -8,5 +24,5 @@ app = Flask(__name__)
 
 if __name__ == '__main__':
 
-    app.run(debug=True, port=30000)
+    app.run(debug=True)
     # app.run(host= '0.0.0.0', port=8081)
